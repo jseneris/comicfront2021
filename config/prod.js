13dae0ca6877d74a57311cbd7dd201765b07b6d9
John Seneris <john@covetlyapp.com>
2021-10-18 11:27:48 -0400

    debugging user auth0 issue

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,5 @@ module.exports = {
   comicApi: process.env.COMICAPI_URI,
   azureCdnAddress: process.env.AZURECDN,
   jwtKey: process.env.JWTKEY,
+  AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
 };
